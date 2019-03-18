96e5a250ba0fcfc85dadd2ce27070489b09b076a
Riku Pepponen <riku.pepponen@gmail.com>
2019-03-18 17:15:31 +0200

    BoardManager.cs: Define Count fuction

--- Unity2D_Roguelike/Assets/Scripts/BoardManager.cs
+++ Unity2D_Roguelike/Assets/Scripts/BoardManager.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class BoardManager : MonoBehaviour
 {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count (int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
