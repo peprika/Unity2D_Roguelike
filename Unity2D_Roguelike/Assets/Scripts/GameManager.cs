63318a7c12fac05f58ea9072b241d752312e7ce0
Riku Pepponen <riku.pepponen@gmail.com>
2019-03-18 19:52:41 +0200

    GameManager.cs: Basic definitions

--- Unity2D_Roguelike/Assets/Scripts/GameManager.cs
+++ Unity2D_Roguelike/Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public BoardManager boardScript;
+
+    private int level = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
