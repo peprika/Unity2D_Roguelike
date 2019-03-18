48e62cf6d1121fbd04b8a6cdb760fa136a547a8a
Riku Pepponen <riku.pepponen@gmail.com>
2019-03-18 21:21:38 +0200

    GameManager.cs: Make GameManager a singelton

--- Unity2D_Roguelike/Assets/Scripts/GameManager.cs
+++ Unity2D_Roguelike/Assets/Scripts/GameManager.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
     public BoardManager boardScript;
 
     private int level = 3;
 
-
     // Use this for initialization
     void Awake()
     {
+        // Make sure there aren't two GameManager instances, and retain it between scenes
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+ 
+        // Get game board and initialize game
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
