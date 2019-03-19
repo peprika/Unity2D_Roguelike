bd8e28301af9da5b245d9353639122ead82c33ff
Riku Pepponen <riku.pepponen@gmail.com>
2019-03-19 23:23:23 +0200

    MovingObject.cs: Define variables at Start()

--- Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
+++ Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
@@ -15,7 +15,10 @@ public abstract class MovingObject : MonoBehaviour
     // It's "protected virtual" so that it can be overwritten by inheriting classes
     protected virtual void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+
     }
 
     // Update is called once per frame
