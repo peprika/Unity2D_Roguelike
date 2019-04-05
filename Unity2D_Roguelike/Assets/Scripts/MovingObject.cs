f6429998f24e033c999d8382d62b9187766b04cf
Riku Pepponen <riku.pepponen@gmail.com>
2019-04-06 00:01:11 +0300

    MovingObject.cs: Add Move()

--- Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
+++ Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
@@ -21,6 +21,24 @@ public abstract class MovingObject : MonoBehaviour
 
     }
 
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
     // Coroutine for element movement
     // "end" = where to move to
     protected IEnumerator SmoothMovement (Vector3 end)
