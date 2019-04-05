3a857586821d05316db11cfd9b0908d594597dff
Riku Pepponen <riku.pepponen@gmail.com>
2019-04-05 22:12:40 +0300

    MovingObject.cs: Movement from original position towards end position

--- Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
+++ Unity2D_Roguelike/Assets/Scripts/MovingObject.cs
@@ -11,7 +11,7 @@ public abstract class MovingObject : MonoBehaviour
     private Rigidbody2D rb2D;
     private float inverseMoveTime;
 
-    // Start is called before the first frame update
+    // Start() is called before the first frame update
     // It's "protected virtual" so that it can be overwritten by inheriting classes
     protected virtual void Start()
     {
@@ -27,6 +27,10 @@ public abstract class MovingObject : MonoBehaviour
     {
         // Calculate the distance to move
         float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while sqrRemaingDistance > float.Epsilon {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime); 
+        }
     }
 
     // Update is called once per frame
