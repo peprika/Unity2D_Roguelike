968b90267337209d580f8e76dd003dc72ce7466e
Riku Pepponen <riku.pepponen@gmail.com>
2019-04-06 18:53:03 +0300

    Player.cs: Add basic declarations

--- Unity2D_Roguelike/Assets/Scripts/Player.cs
+++ Unity2D_Roguelike/Assets/Scripts/Player.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Player : MovingObject
 {
+    // How much damage chop inflicts to walls
+    public int wallDamage = 1;
+
+    // Food items' point values
+    public int pointsPerFood = 10;
+    public int pointsperSoda = 20;
+    
+    // Stores player's score during a level
+    public int food;
+
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
