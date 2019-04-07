58b5b128340c70e396604d008b5f735c79688d97
Riku Pepponen <riku.pepponen@gmail.com>
2019-04-07 20:17:55 +0300

    Enemy.cs: Basic declarations

--- Unity2D_Roguelike/Assets/Scripts/Enemy.cs
+++ Unity2D_Roguelike/Assets/Scripts/Enemy.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MovingObject
 {
+
+    // Declarations
+    public int playerDamage;    // Damage done to the player (food points)
+
+    private Animator animator;  // Animator
+    private Transform target;   // Player's position, aka enemy's target
+    private bool skipMove;      // skipMove is used so that enemy moves every other turn
+
+
     // Start is called before the first frame update
     void Start()
     {
